3156bab379dc0dec5a0eea0b38fedd43e744ae1a
Serafim Sen <sima.serafim2@mail.ru>
2022-03-24 17:45:43 +0300

    init: project

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>MyTop- наш лучший топ</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://mc.yandex.ru" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
+}
+
+export default MyApp;
