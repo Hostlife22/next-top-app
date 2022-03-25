a58157e5a1ad8ef4ec6afcf044cd98815350b9d7
Serafim Sen <sima.serafim2@mail.ru>
2022-03-25 17:43:17 +0300

    feat: added footer

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,19 +1,18 @@
 import React, { FunctionComponent } from "react";
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
+import styles from "./Layout.module.css";
 import { LayoutProps } from "./Layout.props";
 import { Sidebar } from "./Sidebar/Sidebar";
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>{children}</div>
-      </div>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>{children}</div>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
