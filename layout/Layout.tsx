ff934bef30caed7d68682763139ec7ccb61319b7
Serafim Sen <sima.serafim2@mail.ru>
2022-03-25 14:31:43 +0300

    fear: added HOC

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 import { LayoutProps } from "./Layout.props";
 import { Sidebar } from "./Sidebar/Sidebar";
 
-export const Layout = ({ children }: LayoutProps): JSX.Element => {
+const Layout = ({ children }: LayoutProps): JSX.Element => {
   return (
     <>
       <Header />
@@ -15,4 +15,16 @@ export const Layout = ({ children }: LayoutProps): JSX.Element => {
       <Footer />
     </>
   );
+};
+
+export const withLayout = <T extends Record<string, unknown>>(
+  Component: FunctionComponent<T>
+) => {
+  return function withLayoutComponent(props: T): JSX.Element {
+    return (
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    );
+  };
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { Button, Htag, P, Raiting, Tag } from "../components";
-import { Layout } from "../layout/Layout";
+import { withLayout } from "../layout/Layout";
 
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
-    <Layout>
+    <>
       <Htag tag="h2">Заголовок</Htag>
       <Button appearance="primary" arrow="down">
         Кнопка
@@ -26,6 +26,8 @@ export default function Home(): JSX.Element {
       </Tag>
       <Tag color="primary">primary</Tag>
       <Raiting rating={rating} setRating={setRating} isEditable />
-    </Layout>
+    </>
   );
 }
+
+export default withLayout(Home);
