0ec4213319f6cbde29e23792a2d3273ee998156e
Serafim Sen <sima.serafim2021@mail.ru>
2022-03-28 16:27:45 +0300

    feat: added custom hook useScroll

---
+++ hooks/useScrollY.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+export const useScrollY = (): number => {
+  const [scrollY, setScrollY] = useState<number>(0);
+
+  const isBrowser = typeof window !== undefined;
+
+  const handleScroll = (): void => {
+    const currentScrollY = isBrowser ? window.scrollY : 0;
+    setScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return scrollY;
+};
